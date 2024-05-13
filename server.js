d38459bbdc1379436dbe2819a2c418a3636307f8
zoepje <zoe.penner@hva.nl>
2024-05-13 11:57:52 +0200

    server update

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
 /*** Express setup & start ***/
-
-// Importeer de zelfgemaakte functie fetchJson uit de ./helpers map
 import fetchJson from './helpers/fetch-json.js'
 
-// Importeer het npm pakket express uit de node_modules map
 import express, { response } from 'express'
 
-// Maak een nieuwe express app aan
 const app = express()
 
-// Stel ejs in als template engine
 app.set('view engine', 'ejs')
 
-// Stel de map met ejs templates in
 app.set('views', './views')
 
-// Gebruik de map 'public' voor statische resources, zoals stylesheets, afbeeldingen en client-side JavaScript
 app.use(express.static('public'))
 
-// Zorgt dat werken met request data makkelijker wordt
 app.use(express.urlencoded({extended: true}))
 
-// Stel het poortnummer in waar express op moet gaan luisteren
 app.set('port', process.env.PORT || 8000)
 
-// Start express op, haal daarbij het zojuist ingestelde poortnummer op
 app.listen(app.get('port'), function () {
-  // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/*** Variabelen ***/ 
+const redpersUrl = 'https://redpers.nl/wp-json/wp/v2/',
+      directusUrl = 'https://fdnd-agency.directus.app/items/redpers_shares',
+      postsUrl = redpersUrl + 'posts',
+      categoriesUrl = redpersUrl + 'categories',
+      authorUrl = redpersUrl + 'users',
+      categoriesData = [
+        {"id": 9, "name": "Binnenland", "slug": "binnenland"},
+        {"id": 1010, "name": "Buitenland", "slug": "buitenland"}, 
+        {"id": 7164, "name": "Column", "slug": "column"},
+        {"id": 6, "name": "Economie", "slug": "economie"},
+        {"id": 4, "name": "Kunst & Media", "slug": "kunst-media"},
+        {"id": 3211, "name": "Podcasts", "slug": "podcast"},
+        {"id": 63, "name": "Politiek", "slug": "politiek"},
+        {"id": 94, "name": "Wetenschap", "slug": "wetenschap"},
+      ];
+
+/*** Routes & data ***/
