7962e60f5b3bb81418d6902c72baaca2dbd875fb
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-05-14 12:56:06 +0200

    update header

--- public/client.js
+++ public/client.js
@@ -1,7 +1,17 @@
 const nav = document.querySelector("nav.categories-nav"),
-menuButton = document.querySelector(".menu-button");
+menuButton = document.querySelector(".menu-button"),
+searchForm = document.getElementById("search-form"),
+searchInput = document.getElementById("search-input");
 
 
 menuButton.addEventListener("click", () => {
     nav.classList.toggle("closed");
+})
+
+searchForm.addEventListener("submit", (event) => {
+    if (searchInput.classList.contains("hidden") || searchInput.value === "") {
+        event.preventDefault()
+        searchInput.classList.remove("hidden")
+        searchInput.focus()
+    }
 })
