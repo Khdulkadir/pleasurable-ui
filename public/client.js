1f03b4d9785c08ac42c6f4fed5e8325459db92ba
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-05-14 21:19:40 +0200

    update search page

--- public/client.js
+++ public/client.js
@@ -2,6 +2,7 @@ const nav = document.querySelector("nav.categories-nav"),
 menuButton = document.querySelector(".menu-button"),
 searchForms = document.querySelectorAll(".search-form"),
 searchInputs = document.querySelectorAll(".search-input"),
+searchterm = document.getElementById("searchterm"),
 header2 = document.querySelector(".header2"),
 header3 = document.querySelector(".header3");
 
@@ -10,21 +11,9 @@ menuButton.addEventListener("click", () => {
     nav.classList.toggle("closed");
 })
 
-//Code voor search form
-searchForms.forEach((searchForm, i) => { //Omdat er 2 search forms zijn doe ik ze in for each loop anders krijg je dubbele code
-    searchForm.addEventListener("submit", (event) => {
-        const searchInput = searchInputs[i] //Iteratie van de loop
-        if (searchInput.classList.contains("hidden") || searchInput.value === "") { //Als search input hidden of leeg is
-            event.preventDefault()
-            searchInput.classList.remove("hidden")
-            searchInput.focus()
-        }
-    })
-})
-
 // Code voor sticky header3 
 const observer = new IntersectionObserver(([{isIntersecting}], _) => { //Dit gebeurd wanneer header2 het scherm binnenkomt of verlaat
-    if (isIntersecting) { // Als header2 zichtbaar wordt
+    if (isIntersecting) { // Als header2 zichtbaar is
         header3.classList.remove("fixed") //Zet header3 op geen fixed
     } else { //Als header2 weggaat
         header3.classList.add("fixed") //Zet header3 op fixed
