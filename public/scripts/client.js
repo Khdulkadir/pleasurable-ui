c8de896c447792e2ff2fe0f54c51a6bb96df82e2
Shanna van der Werve <shanna.van.der.werve@hva.nl>
2024-05-27 15:35:47 +0200

    darkmode and header done

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,46 @@ const nav = document.querySelector("nav.categories-nav"),
       header2 = document.querySelector(".header2"),
       header3 = document.querySelector(".header3"),
       progressBar = document.querySelector("#myBar"),
-      forms = document.querySelectorAll("form#like-form");
+      forms = document.querySelectorAll("form#like-form"),
+      darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)"),
+      lightIcon = document.getElementById("light-icon"),
+      darkIcon = document.getElementById("dark-icon");
+
+let darkMode = darkModeMediaQuery.matches;
+
+darkModeMediaQuery.addEventListener("change", (e) => {
+  if (e.matches) {
+    darkMode = true;
+  } else {
+    darkMode = false;
+  }
+});
+
+// Set dark-mode class on body if darkMode is true and pick icon
+if (darkMode) {
+  document.body.classList.add("dark-mode");
+  darkIcon.setAttribute("display", "none");
+} else {
+  lightIcon.setAttribute("display", "none");
+}
+
+// Toggle dark mode on button click
+function toggleDarkMode() {
+  // Toggle darkMode variable
+  darkMode = !darkMode;
+
+  // Toggle dark-mode class on body
+  document.body.classList.toggle("dark-mode");
+
+  // Toggle light and dark icons
+  if (darkMode) {
+    lightIcon.setAttribute("display", "block");
+    darkIcon.setAttribute("display", "none");
+  } else {
+    lightIcon.setAttribute("display", "none");
+    darkIcon.setAttribute("display", "block");
+  }
+}
 
 menuButton.addEventListener("click", () => {
     nav.classList.toggle("closed");
